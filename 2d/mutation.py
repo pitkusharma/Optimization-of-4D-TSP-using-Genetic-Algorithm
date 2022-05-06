48498719baff6fb45bd057a474527b9f299d142e
pitkusharma <48028965+pitkusharma@users.noreply.github.com>
2022-05-06 21:51:37 +0530

    Update mutation.py

--- 2d/mutation.py
+++ 2d/mutation.py
@@ -1,5 +1,15 @@
 import random
-from data_functions import distinct_checking
+
+
+def distinct_checking(populations, index):
+    if index > 0:
+        temp_index = index - 1
+        while temp_index >= 0:
+            if populations[index] == populations[temp_index]:
+                random.shuffle(populations[index])
+                temp_index = index - 1
+            else:
+                temp_index -= 1
 
 
 def inversion_mutation(individual):
@@ -23,22 +33,10 @@ def inversion_mutation(individual):
     random_subtour, p1, p2 = subtour(individual.route)
     individual.route[p1 : p2+1] = random_subtour[:]
 
-    random_subtour, p1, p2 = subtour(individual.road)
-    individual.road[p1 : p2+1] = random_subtour[:]
-
-    random_subtour, p1, p2 = subtour(individual.vehicle)
-    individual.vehicle[p1 : p2+1] = random_subtour[:]
-
 
 def insertion_mutation(individual):
     temp = individual.route.pop(random.randint(0, len(individual.route)-1))
     individual.route.insert(random.randint(0, len(individual.route)-1), temp)
-
-    temp = individual.road.pop(random.randint(0, len(individual.road)-1))
-    individual.road.insert(random.randint(0, len(individual.road)-1), temp)
-
-    temp = individual.vehicle.pop(random.randint(0, len(individual.vehicle)-1))
-    individual.vehicle.insert(random.randint(0, len(individual.vehicle)-1), temp)
 
 
 def exchange_mutation(individual):
@@ -56,21 +54,13 @@ def exchange_mutation(individual):
     individual.route[p1] = individual.route[p2]
     individual.route[p2] = temp
     
-    p1, p2 = point_picker(individual.road)
-    temp = individual.road[p1]
-    individual.road[p1] = individual.road[p2]
-    individual.road[p2] = temp
-
-    p1, p2 = point_picker(individual.vehicle)
-    temp = individual.vehicle[p1]
-    individual.vehicle[p1] = individual.vehicle[p2]
-    individual.vehicle[p2] = temp
-
 
 def heuristic_mutation(individual):
     three_points = []
     permutes = []
     temp_route = individual.route.copy()
+
+    min_dist = individual.distance
 
     for i in range(0, 3):
         temp = random.randint(0, len(temp_route)-1)
@@ -87,17 +77,18 @@ def heuristic_mutation(individual):
         permutes.append(random.sample(random_cities, 3))
         distinct_checking(permutes, i)
 
-
     back_up = individual.route.copy()
+    
     for item in permutes:
         temp_route[three_points[0]] = item[0]
         temp_route[three_points[1]] = item[1]
         temp_route[three_points[2]] = item[2]
 
         individual.route = temp_route
-        individual.cost(individual)
-        if individual.cost < min:
-            individual.route = temp_route.copy()
-        else:
-            individual.route = back_up.copy()
+        individual.distance = fitnessOperator(individual)
 
+        if individual.distance < min_dist:
+          individual.route = temp_route.copy()
+          back_up = temp_route.copy()
+        else:
+          individual.route = back_up.copy()
